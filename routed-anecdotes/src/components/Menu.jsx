41abee825fe1955f20a659dd5858bc3ac3bafefd
Fyscher Kraal <fyscher.kraal@gmail.com>
2025-08-29 09:22:51 -0600

    initial commit

---
+++ routed-anecdotes/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+
+const Menu = () =>
+{
+
+    return(
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/anecdotes">Anecdotes</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/about">About</Link>
+            <Link to="/new">Create New</Link>
+        </div>
+    )
+}
+
+export default Menu;
